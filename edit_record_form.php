5968b645198ac3c36b13eeadfb3f41ceb1945610
Ted <tadas2410@gmail.com>
2021-02-16 12:57:13 +0000

    changed sidebar to be an include

--- edit_record_form.php
+++ edit_record_form.php
@@ -12,44 +12,40 @@ $records = $statement->fetch(PDO::FETCH_ASSOC);
 $statement->closeCursor();
 ?>
 <!-- the head section -->
- <div class="container">
-<?php
-include('includes/header.php');
-?>
-        <h1>Edit Product</h1>
-        <form action="edit_record.php" method="post" enctype="multipart/form-data"
-              id="add_record_form">
-            <input type="hidden" name="original_image" value="<?php echo $records['image']; ?>" />
-            <input type="hidden" name="record_id"
-                   value="<?php echo $records['recordID']; ?>">
+<div class="container">
+       <?php
+       include('includes/header.php');
 
-            <label>Category ID:</label>
-            <input type="category_id" name="category_id"
-                   value="<?php echo $records['categoryID']; ?>">
-            <br>
+       ?>
+       <h1>Edit Product</h1>
+       <form action="edit_record.php" method="post" enctype="multipart/form-data" id="add_record_form">
+              <input type="hidden" name="original_image" value="<?php echo $records['image']; ?>" />
+              <input type="hidden" name="record_id" value="<?php echo $records['recordID']; ?>">
 
-            <label>Name:</label>
-            <input type="input" name="name"
-                   value="<?php echo $records['name']; ?>">
-            <br>
+              <label>Category ID:</label>
+              <input type="category_id" name="category_id" value="<?php echo $records['categoryID']; ?>">
+              <br>
 
-            <label>List Price:</label>
-            <input type="input" name="price"
-                   value="<?php echo $records['price']; ?>">
-            <br>
+              <label>Name:</label>
+              <input type="input" name="name" value="<?php echo $records['name']; ?>">
+              <br>
 
-            <label>Image:</label>
-            <input type="file" name="image" accept="image/*" />
-            <br>            
-            <?php if ($records['image'] != "") { ?>
-            <p><img src="image_uploads/<?php echo $records['image']; ?>" height="150" /></p>
-            <?php } ?>
-            
-            <label>&nbsp;</label>
-            <input type="submit" value="Save Changes">
-            <br>
-        </form>
-        <p><a href="index.php">View Homepage</a></p>
-    <?php
-include('includes/footer.php');
-?>
+              <label>List Price:</label>
+              <input type="input" name="price" value="<?php echo $records['price']; ?>">
+              <br>
+
+              <label>Image:</label>
+              <input type="file" name="image" accept="image/*" />
+              <br>
+              <?php if ($records['image'] != "") { ?>
+                     <p><img src="image_uploads/<?php echo $records['image']; ?>" height="150" /></p>
+              <?php } ?>
+
+              <label>&nbsp;</label>
+              <input type="submit" value="Save Changes">
+              <br>
+       </form>
+       <p><a href="index.php">View Homepage</a></p>
+       <?php
+       include('includes/footer.php');
+       ?>
